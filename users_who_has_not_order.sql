6d34b7622a389e8494e910dbd455a6d476a39fd4
Bamidele Adeola <ajamubamidele@gmail.com>
2022-11-20 14:12:37 +0100

    Users_without_orders

---
+++ users_who_has_not_order.sql
@@ -0,0 +1,11 @@
+SELECT * FROM simplehealth.orders;
+
+-- 2. Which of our users have not placed any orders?
+SELECT 
+    u.id
+FROM users u
+WHERE
+    u.id NOT IN (SELECT 
+            user_id
+        FROM
+            orders);
